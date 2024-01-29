99f8fc4088781349cbe83935373df1238379f029
midnightmastermind <josh@jpoms.com>
2024-01-29 13:21:57 -0600

    test3

--- back-end/packages/ecommerce/models/product2.model.js
+++ back-end/packages/ecommerce/models/product2.model.js
@@ -1,32 +1,100 @@
 const mongoose = require('mongoose');
-const { Types } = mongoose;
-// const Grid = require('gridfs-stream');
-
-// const conn = mongoose.createConnection('mongodb://localhost/my_database'); // Replace with your MongoDB connection string
-
-// let gfs;
-// conn.once('open', () => {
-//   // Init stream
-//   gfs = Grid(conn.db, mongoose.mongo);
-//   gfs.collection('photos'); // Set the collection name
-// });
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  color: String,
-  status: { type: String, enum: ['publish', 'not'], default: 'publish' },
-  price: { type: Number, required: true },
-  tax: { type: Number, required: true },
-  description: String,
-  dimensions: {
-    width: { type: Number, required: true },
-    height: { type: Number, required: true },
-    depth: { type: Number, required: true },
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => mongoose.Types.ObjectId("65b7dc54cf74c369c84be2b3")
   },
-  photo: { type: Types.ObjectId, ref: 'Photo' }, // Use ObjectId reference
-  // Add more fields as needed
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
+  ID: {
+    type: Number,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ["variable"]
+  },
+  SKU: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  published: {
+    type: Number,
+    required: true
+  },
+  isFeatured: {
+    type: Number,
+    required: true
+  },
+  visibilityInCatalog: {
+    type: String,
+    required: true,
+    enum: ["visible"]
+  },
+  shortDescription: {
+    type: String,
+    required: true
+  },
+  productDescription: {
+    type: String,
+    required: true
+  },
+  inStock: {
+    type: Number,
+    required: true
+  },
+  stock: {
+    type: Number,
+    required: true
+  },
+  backordersAllowed: {
+    type: Number,
+    required: true
+  },
+  soldIndividually: {
+    type: Number,
+    required: true
+  },
+  allowCustomerReviews: {
+    type: Number,
+    required: true
+  },
+  regularPrice: {
+    type: Number,
+    required: true
+  },
+  categories: {
+    type: String,
+    required: true
+  },
+  images: {
+    type: String,
+    required: true
+  },
+  attribute1Name: {
+    type: String,
+    required: true
+  },
+  attribute1Values: {
+    type: [String],
+    required: true
+  },
+  attribute2Name: {
+    type: String,
+    required: true
+  },
+  attribute2Values: {
+    type: [String],
+    required: true
+  },
+  weight: {
+    type: Number,
+    required: true
+  }
 });
 
 const Product = mongoose.model('Product', productSchema);
